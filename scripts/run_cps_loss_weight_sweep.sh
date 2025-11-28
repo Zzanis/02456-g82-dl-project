1fc38b5ab44aa0a976ddfafeb35204f004ef5383
Migle Cerniauskaite <nebaisu@gmail.com>
2025-11-28 09:38:02 +0100

    Update CPS loss weight sweeps

--- scripts/run_cps_loss_weight_sweep.sh
+++ scripts/run_cps_loss_weight_sweep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # BSUB job array: submit with `bsub < scripts/run_lr_sweep.sh`
 # Adjust learning rates in LR_VALUES below.
-#BSUB -J dl_project_g82_cps_loss_weight[1-6]
+#BSUB -J dl_project_g82_cps_loss_weight[1-15]
 #BSUB -q gpuv100
 #BSUB -W 1:00
 #BSUB -n 4
@@ -19,7 +19,8 @@ source $DL_PROJECT_CONFIG_FILE
 REPO=${DL_PROJECT_REPO_DIR:?Missing variable}
 RUN_DIR=${DL_PROJECT_RUN_DIR:-$DL_PROJECT_REPO_DIR}
 
-CPS_LOSS_WEIGHT_VALUES=(0 0.5 1 1.5 2 5)
+# CPS_LOSS_WEIGHT_VALUES=(0 0.5 1 1.5 2 5)
+CPS_LOSS_WEIGHT_VALUES=(0 0.25 0.5 0.75 1 1.25 1.5 1.75 2 2.25 2.5 2.75 3 4 5)
 CPS_LOSS_WEIGHT=${CPS_LOSS_WEIGHT_VALUES[$((LSB_JOBINDEX-1))]}
 
 echo "Project repo dir: $REPO"
