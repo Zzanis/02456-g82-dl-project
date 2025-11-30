77e8b19166a12bed63105ba8f5c531800c490d79
Migle Cerniauskaite <nebaisu@gmail.com>
2025-11-30 12:42:50 +0100

    Update job scipt

--- scripts/gnn-run-gpu.sh
+++ scripts/gnn-run-gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#BSUB -J miglec-DLProjectGNN
+#BSUB -J miglec-dl
 #BSUB -q gpuv100
-#BSUB -R "rusage[mem=5GB]"
+#BSUB -R "rusage[mem=4GB]"
 #BSUB -B
 #BSUB -N
 #BSUB -o /zhome/2b/a/223921/runs/02456-g82-dl-project/Output_%J.out
@@ -10,18 +10,9 @@
 # Need to specify at least 4 cores when using GPU queue
 #BSUB -n 4
 # Select the resources: 1 gpu in exclusive process mode
-#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -gpu "num=1"
+##BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "span[hosts=1]"
-
-# use this for unbuffered output, so that you can check in real-time
-# (with tail -f Output_.out Output_.err)
-# what your program was printing "on the screen"
-# python3 -u helloworld.py
-
-# use this for just piping everything into a file, 
-# the program knows then, that it's outputting to a file
-# and not to a screen, and also combine stdout&stderr
-# python3 helloworld.py > joboutput_$LSB_JOBID.out 2>&1
 
 
 REPO=/zhome/2b/a/223921/projects/02456-g82-dl-project
